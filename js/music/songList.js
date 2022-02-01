d0732aaf912b3d231c1e1879b77f0b87ca396b66
Y_ALANG <92966060+yalwolf@users.noreply.github.com>
2022-02-01 14:08:45 +0800

    Add files via upload

---
+++ js/music/songList.js
@@ -0,0 +1,12 @@
+/**
+ * Created by smohan on 2016/11/19.
+ */
+var songList = [
+    {
+        title: '2019无敌大串烧',
+        singer: '蛋白后宫、蛋白水友',
+        audio: 'http://music.dbhg.top/music/music.mp3',
+        thumbnail: 'https://imgessl.kugou.com/stdmusic/20210114/20210114231002779847.jpg',
+        lyric: "http://api.dbhg.top/music/lrc/2019.lrc"
+    },
+];
