cff088a9d5cfb526f413edebb93213959cec0f98
Y_ALANG <92966060+yalwolf@users.noreply.github.com>
2022-10-22 11:41:20 +0800

    Add files via upload

---
+++ js/alapi_log.js
@@ -0,0 +1,5 @@
+window.version = 1.0.2
+console.log("%cAL-API%cversion - " + window.version,
+    "color:#000;background:#0099FF;padding:3px 7px;border-top-left-radius: 3px;border-bottom-left-radius: 3px;",
+    "color:#000;background:#FF9900;padding:3px 7px;border-top-right-radius: 3px;border-bottom-right-radius: 3px;"
+)
